e2aa3a94142b5c5e6499379412d16fb07ada21d3
ImabubblyIron <satls34@naver.com>
2020-01-16 01:44:54 +0900

    [com.iron.base] BaseViewHolder 추가

---
+++ app/src/main/java/com/iron/base/BaseViewHolder.kt
@@ -0,0 +1,19 @@
+package com.iron.base
+
+import android.content.Context
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+open class BaseViewHolder<T>(
+    private val viewDataBinding: ViewDataBinding
+) : RecyclerView.ViewHolder(viewDataBinding.root) {
+
+    protected val context : Context = itemView.context
+
+    protected open fun onBind(item : T?) {
+        viewDataBinding.apply {
+            //setVariable(BR.item, item)
+            executePendingBindings()
+        }
+    }
+}
